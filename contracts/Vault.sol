c541f7a2ab29ffd5a34e53163b191b435fb40c42
hassan <salakohassanmobolaji@gmail.com>
2026-06-19 10:11:29 +0100

    feat: implement ERC4626 vault with yield distribution, Inherit from ERC4626 standard,Implement deposit, withdraw, mint, redeem,Add MockUSDC for testing, Create distributeYield() to demonstrate yield accrual,Tests cover deployment and yield distribution

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-contract Vault is ERC20 { constructor() ERC20('MockVault', 'MVT') {} }
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract MockUSDC is ERC20 {
+    constructor() ERC20('Mock USDC', 'USDC') {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
+
+contract Vault is ERC4626, Ownable {
+    constructor(address _asset) 
+        ERC20('Yield Vault', 'yVault')
+        ERC4626(IERC20(_asset))
+    {}
+
+    function distributeYield(uint256 amount) external onlyOwner {
+        require(amount > 0, 'Amount must be > 0');
+       IERC20(asset()).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return super.decimals();
+    }
+}
